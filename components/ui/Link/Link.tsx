fac68c0e0a1e0eb90394f08f00bbb89e4a30b70d
Maksym Mykhailenko <maxceem@gmail.com>
2021-08-25 21:30:04 +0300

    smooth scrolling by anchors

--- components/ui/Link/Link.tsx
+++ components/ui/Link/Link.tsx
@@ -1,11 +1,57 @@
-import NextLink, { LinkProps as NextLinkProps } from 'next/link'
+import NextLink, { LinkProps } from 'next/link';
+import { FC, useCallback } from 'react';
 
-const Link: React.FC<NextLinkProps> = ({ href, children, ...props }) => {
+/**
+ * NextLink component which supports smooth scrolling
+ */
+const Link: FC<LinkProps> = ({
+  children,
+  href,
+  as,
+  replace,
+  scroll,
+  shallow,
+  passHref,
+  prefetch,
+  locale,
+  ...anchorProps
+}) => {
+  const handleClick = useCallback(
+    (e) => {
+      if ((href as string).startsWith('#')) {
+        e.preventDefault();
+        const destination = document.querySelector(href as string);
+        if (destination && typeof destination.scrollIntoView === 'function') {
+          destination.scrollIntoView({
+            behavior: 'smooth',
+          });
+        }
+      }
+    },
+    [href]
+  );
   return (
-    <NextLink href={href}>
-      <a {...props}>{children}</a>
+    <NextLink
+      href={href}
+      as={as}
+      replace={replace}
+      scroll={scroll}
+      shallow={shallow}
+      passHref={passHref}
+      prefetch={prefetch}
+      locale={locale}
+    >
+      <a
+        tabIndex={0}
+        role="link"
+        onClick={handleClick}
+        onKeyDown={handleClick}
+        {...anchorProps}
+      >
+        {children}
+      </a>
     </NextLink>
-  )
-}
+  );
+};
 
-export default Link
+export default Link;
